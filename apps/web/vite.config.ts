dede63570121cef5605c00f71b66e6b63275308c
deepackayub023-cyber <deepackayub023@gmail.com>
2026-07-27 21:57:16 +0300

    fix outDir

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../../dist/apps/web',
+  },
   server: {
     port: 4200,
     proxy: {
